d317a4edf64630d09f10de7b3c9aa5384d4e8a9e
Gwann <682024031@student.uksw.edu>
2024-11-13 19:31:30 +0700

    Add files via upload

---
+++ TA_PTI2024/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed(".text-animation", {
+    strings: ["Back End Developer", "Ultraman", "Guitarist", "EXP Laner", "Sigma", "Mechanic"],
+    typeSpeed: 80,
+    backSpeed:100,
+    backDelay: 1000,
+    loop: true
+  })
+})
+
+  
